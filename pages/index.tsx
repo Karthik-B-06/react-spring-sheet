90b5412c85c5130e3a21405a6966901c5a6f3ed2
Karthik-B-06 <karthik06051996@gmail.com>
2022-10-10 16:47:11 +0530

    feat: :sparkles: initial code for sheet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,46 @@
-import { Button } from "../components";
+import { PropsWithChildren } from "react";
+import { useDrag } from "@use-gesture/react";
+import { Portal } from "ariakit";
 
 export default function Home() {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <h1>
-        <p className="font-extrabold">Next React App</p>
-      </h1>
-      <Button>Hi</Button>
+    <div className="flex h-screen flex-1 justify-center items-center">
+      <button className="bg-blue-700 px-2 py-3 uppercase text-white rounded-lg">
+        open sheet
+      </button>
+      <Sheet />
     </div>
   );
 }
+
+const Sheet: React.FC = () => {
+  return (
+    <Portal>
+      <SheetWrapper>
+        <SheetDraggable />
+        <div>Sheet Header</div>
+      </SheetWrapper>
+    </Portal>
+  );
+};
+
+const SheetDraggable: React.FC = () => {
+  const bind = useDrag(
+    state => {
+      console.log("%c⧭", "color: #ff0000", state);
+    },
+    { axis: "y" },
+  );
+  return (
+    <div
+      {...bind}
+      className="flex flex-row flex-1 justify-center items-center py-2"
+    >
+      <div className="w-5 h-1.5 rounded-full bg-gray-500"></div>
+    </div>
+  );
+};
+
+const SheetWrapper: React.FC<PropsWithChildren> = ({ children }) => {
+  return <div className="fixed bottom-0">{children}</div>;
+};
